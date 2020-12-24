005d2c486b6107a7d660e671c5eb493809e3d11d
IdanAlbilia <albilia14@gmail.com>
2020-12-24 11:55:20 +0200

    updated - dropped invalid constraints

--- OCL_Validator.py
+++ OCL_Validator.py
@@ -2,29 +2,30 @@ import subprocess
 from DAO import DAO
 
 dao = DAO()
-# dao.ChangeDB('OCLTest.db')
-constraints = dao.Get_Expressions_For_Validation()
-# counter = 1
-Validation_Constraints_TXT = open("Constraints_To_Validate.txt",'a',encoding="utf-8")
-for Exp in constraints:
-    expression = str(Exp[2]).strip('\n')
-    expression = expression.strip('\t')
-    expression = expression.strip('\r')
-    expression = expression.replace('\n',"")
-    expression = expression.replace('\t',"")
-    expression = expression.replace('\r',"")
-    Validation_Constraints_TXT.write("@" + str(Exp[0]) + "#" + str(Exp[3]) + "#" + str(Exp[1])
-                                     + "#" + str(Exp[4]) + "#" + expression + "\n")
 
-#
-#
-# for Exp in constraints:
-#     Command = "java -jar OCLtoAST.jar " + str(Exp[1].replace(" ",""))
-#     Output = subprocess.run(Command, capture_output=True, text=True)
-#     print(str(counter)+" - "+Output.stdout)
-#     counter += 1
-#     dao.AddAST(Exp[0], Output.stdout)
-#
-# Command = "java -jar OCLtoAST.jar " + "region->size()=0"
-# Output = subprocess.run(Command, capture_output=True, text=True)
-# print(Output.stdout)
+
+def export_constraints_to_txt():
+    constraints = dao.Get_Expressions_For_Validation()
+    Validation_Constraints_TXT = open("Constraints_To_Validate.txt",'a',encoding="utf-8")
+    for Exp in constraints:
+        expression = str(Exp[2]).strip('\n')
+        expression = expression.strip('\t')
+        expression = expression.strip('\r')
+        expression = expression.replace('\n',"")
+        expression = expression.replace('\t',"")
+        expression = expression.replace('\r',"")
+        Validation_Constraints_TXT.write("@" + str(Exp[0]) + "#" + str(Exp[3]) + "#" + str(Exp[1])
+                                         + "#" + str(Exp[4]) + "#" + expression + "\n")
+
+
+def delete_invalid_constraints_from_db():
+    invalid_ocl = open('invalidOCL.txt', 'r')
+    Lines = invalid_ocl.readlines()
+    count = 0
+    # Strips the newline character
+    for line in Lines:
+        constraint_id = line.split(',')[0]
+        dao.delete_invalid_constraints(constraint_id)
+        print(constraint_id)
+
+delete_invalid_constraints_from_db()
