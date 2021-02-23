5beacca9bda4e35f670a20a0aafbc02cb424e599
noaweiss25 <weissf@post.bgu.ac.il>
2021-02-23 12:09:39 +0200

    classifier minor changes

--- Classifier.py
+++ Classifier.py
@@ -11,6 +11,8 @@ from time import time
 from scipy.stats import entropy
 import numpy as np
 from imblearn.over_sampling import RandomOverSampler
+from imblearn.under_sampling import RandomUnderSampler
+
 from datetime import datetime
 from sklearn.feature_selection import mutual_info_classif
 from sklearn.model_selection import train_test_split
@@ -41,22 +43,22 @@ def inherits_column(value):
         return 1
 
 #
-def createBalancedData():
-    NoCons_indices = df[df.ContainsConstraints == 0].index
-    Cons_indices = df[df.ContainsConstraints == 1].index
-    random_NoCons = np.random.choice(NoCons_indices, 3005, replace=False)
-    RandomNoCons_sample = df.loc[random_NoCons]
-    Constraints_sample = df.loc[Cons_indices]
-    return pd.concat([RandomNoCons_sample, Constraints_sample], ignore_index=True)
-
-def createBalancedDataRef():
-    NoCons_indices = df[df.Referenced == 0].index
-    Cons_indices = df[df.Referenced == 1].index
-    # random_NoCons = np.random.choice(NoCons_indices, 3572, replace=False)
-    random_NoCons = np.random.choice(NoCons_indices, 10000, replace=False)
-    RandomNoCons_sample = df.loc[random_NoCons]
-    Constraints_sample = df.loc[Cons_indices]
-    return pd.concat([RandomNoCons_sample, Constraints_sample], ignore_index=True)
+# def createBalancedData():
+#     NoCons_indices = df[df.ContainsConstraints == 0].index
+#     Cons_indices = df[df.ContainsConstraints == 1].index
+#     random_NoCons = np.random.choice(NoCons_indices, 3005, replace=False)
+#     RandomNoCons_sample = df.loc[random_NoCons]
+#     Constraints_sample = df.loc[Cons_indices]
+#     return pd.concat([RandomNoCons_sample, Constraints_sample], ignore_index=True)
+#
+# def createBalancedDataRef():
+#     NoCons_indices = df[df.Referenced == 0].index
+#     Cons_indices = df[df.Referenced == 1].index
+#     # random_NoCons = np.random.choice(NoCons_indices, 3572, replace=False)
+#     random_NoCons = np.random.choice(NoCons_indices, 10000, replace=False)
+#     RandomNoCons_sample = df.loc[random_NoCons]
+#     Constraints_sample = df.loc[Cons_indices]
+#     return pd.concat([RandomNoCons_sample, Constraints_sample], ignore_index=True)
 
 def drop_columns(Final):
     Final = Final.drop('ModelID', axis=1)
@@ -70,6 +72,7 @@ def drop_columns(Final):
     Final = Final.drop('inheriting_from', axis=1)
     # Final = Final.drop('ReferencedInConstraint', axis=1)
     # Final = Final.drop('is_abstract', axis=1)
+    # Final = Final.drop('inherits', axis=1)
     return Final
 
 def featureImportance(feature_names):
@@ -82,6 +85,23 @@ def featureImportance(feature_names):
     plt.xticks(range(X.shape[1]), names, rotation=20, fontsize=8)
     plt.title("Feature Importance")
     plt.show()
+
+def feature_imp(Final):
+    rnd_clf = RandomForestClassifier(n_estimators=500, n_jobs=-1, random_state=42)
+    rnd_clf.fit(X,y)
+    for name, importance in zip(Final.columns, rnd_clf.feature_importances_):
+        ...
+        print(name, "=", importance)
+    features = Final.columns
+    importances = rnd_clf.feature_importances_
+    indices = np.argsort(importances)
+
+    plt.title('Feature Importances')
+    plt.barh(range(len(indices)), importances[indices], color='b', align='center')
+    plt.yticks(range(len(indices)), [features[i] for i in indices])
+    plt.xlabel('Relative Importance')
+    plt.show()
+
 
 def classify():
     models = [ GaussianNB(),KNeighborsClassifier(),RandomForestClassifier(n_estimators=200, random_state=1, class_weight='balanced') ]
@@ -98,27 +118,25 @@ def classify():
 DAO = DAO()
 df = DAO.getObjects()
 
-# Target Value Column
 df['inherits'] = df.apply(lambda x: inherits_column(x['inheriting_from']), axis=1)
+
+# Target Value Column
 df['ContainsConstraints'] = df.apply(lambda x: CheckifConstraint(x['ConstraintsNum']), axis=1)
 # df['Referenced'] = df['ReferencedInConstraint'].fillna(0)
 
-# Create Balanced Dataframe 1:2 ratio / 1:1 ratio
-Final = createBalancedData()
-print( "Number of Rows in final dataframe is : " +str(Final.shape))
+print(df[df['ContainsConstraints'] == 1 ].shape)
+Final = df
 Final = drop_columns(Final)
 X = Final.iloc[:, :-1].values
 y = Final.iloc[:, -1].values
 print()
 print("--------------------Mutual Information----------------------")
 feature_names = Final.columns
-res = mutual_info_classif(X, y, discrete_features=True)
-print(X)
+res = mutual_info_classif(X, y)
 print(dict(zip(feature_names, res)))
 
-
-oversample = RandomOverSampler(sampling_strategy='minority')
-# oversample = RandomOverSampler(sampling_strategy=0.5)
+# equal ratio of 0 and 1 for target variable
+oversample = RandomUnderSampler()
 
 # fit and apply the transform
 X_over, y_over = oversample.fit_resample(X, y)
@@ -126,9 +144,11 @@ X_over, y_over = oversample.fit_resample(X, y)
 # #Split to Train and Test
 X_train, X_test, y_train, y_test = train_test_split(X_over, y_over, test_size=0.25, random_state=0)
 
+print("--------------------Under Sampling----------------------")
+print( "Number of Rows in Train Set is : " +str(X_train.shape))
+print( "Number of Rows in Test Set is : " +str(X_test.shape))
 
 print("--------------------Results----------------------")
 classify()
-
 
 featureImportance(feature_names)
