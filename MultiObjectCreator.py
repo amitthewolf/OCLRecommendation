fdec614d2a63fc653f67489bbf6e399c89f92d2a
IdanAlbilia <albilia14@gmail.com>
2021-04-20 19:57:08 +0300

    Multi

--- MultiObjectCreator.py
+++ MultiObjectCreator.py
@@ -4,7 +4,6 @@ import pandas as pd
 from itertools import product
 
 
-
 class MultiObjectCreator:
 
     def __init__(self):
@@ -34,47 +33,55 @@ class MultiObjectCreator:
                 # ADDING POSITIVE RECORDS
                 if objs_in_const_number > 1:
                     objects_ids_in_constraint = objs_in_const_df['ObjectID'].tolist()
-                    for positive_subset in itertools.combinations(objects_ids_in_constraint, 2):
-                        x.add(positive_subset) # added
-                        obj_1 = self.df_objects.loc[self.df_objects['ObjectID'] == positive_subset[0]].reset_index(drop=True)
-                        obj_2 = self.df_objects.loc[self.df_objects['ObjectID'] == positive_subset[1]].reset_index(drop=True)
+                    object_ids_in_const_set = set(objects_ids_in_constraint)
+                    for positive_subset in itertools.combinations(object_ids_in_const_set, 2):
+                        x.add(positive_subset)  # added
+                        obj_1 = self.df_objects.loc[self.df_objects['ObjectID'] == positive_subset[0]].reset_index(
+                            drop=True)
+                        obj_2 = self.df_objects.loc[self.df_objects['ObjectID'] == positive_subset[1]].reset_index(
+                            drop=True)
                         if not obj_1.empty and not obj_2.empty:
                             new_df = obj_1.join(obj_2, lsuffix="_1", rsuffix="_2")
                             new_df['PairInConstraint'] = 1
-                            self.my_df = pd.concat([self.my_df,new_df], axis=0)
+                            self.my_df = pd.concat([self.my_df, new_df], axis=0)
                             positive_pairs_ctr += 1
 
             # IF POSITIVE RECORDS ADDED, CREATE ALSO NEGATIVE RECORDS
             if positive_pairs_ctr > 0:
-                combinations = list(product(obj_ids_in_model_with_no_const,obj_ids_in_model_with_consts ,repeat=1))
-                if len(combinations) == 0:
-                    y = set()
-                    for last_subset in itertools.combinations(all_obj_ids_in_model, 2):
-                        y.add(last_subset)
-                    combinations = self.subtract_sets(x, y)
-                    self.my_df = self.my_df[self.my_df['ModelID_1'] != model_id]
-                for negative_subset in combinations:
+                # combinations = list(product(obj_ids_in_model_with_no_const, obj_ids_in_model_with_consts, repeat=1))
+                # if len(combinations) == 0:
+                y = set()
+                for last_subset in itertools.combinations(all_obj_ids_in_model, 2):
+                    y.add(last_subset)
+                combinations = self.subtract_sets(x, y)
+                # if len(combinations) < positive_pairs_ctr:
+                #     self.my_df = self.my_df[self.my_df['ModelID_1'] != model_id]
+                #     continue
+                comb_set = set(combinations)
+                for negative_subset in comb_set:
                     # STOP WHEN POSITIVE RECORDS NUM = NEGATIVE RECORDS NUM
-                    if negative_pairs_ctr == positive_pairs_ctr:
-                        break
-                    obj_1 = self.df_objects.loc[self.df_objects['ObjectID'] == negative_subset[0]].reset_index(drop=True)
-                    obj_2 = self.df_objects.loc[self.df_objects['ObjectID'] == negative_subset[1]].reset_index(drop=True)
+                    # if negative_pairs_ctr == positive_pairs_ctr:
+                    #     break
+                    obj_1 = self.df_objects.loc[self.df_objects['ObjectID'] == negative_subset[0]].reset_index(
+                        drop=True)
+                    obj_2 = self.df_objects.loc[self.df_objects['ObjectID'] == negative_subset[1]].reset_index(
+                        drop=True)
                     new_df = obj_1.join(obj_2, lsuffix="_1", rsuffix="_2")
                     new_df['PairInConstraint'] = 0
                     if not obj_1.empty and not obj_2.empty:
                         self.my_df = pd.concat([self.my_df, new_df], axis=0)
                         negative_pairs_ctr += 1
 
-        self.my_df.to_csv("pairs_df.csv")
+        self.my_df.to_csv("pairs_df.csv", index=False)
 
         final_features = []
         for feature in features:
             final_features.append(feature + str("_1"))
             final_features.append(feature + str("_2"))
 
-
-        print("Unique models number in pairs dataframe",len(self.my_df['ModelID_1'].unique()))
+        print("Unique models number in pairs dataframe", len(self.my_df['ModelID_1'].unique()))
         print("*" * 50)
+        # self.my_df.drop(['ConstraintsNum_1', 'ConstraintsNum_2'], axis=1, inplace=True)
         return self.my_df, final_features
 
     def subtract_sets(self, x, y):
@@ -88,3 +95,4 @@ class MultiObjectCreator:
             else:
                 final_set.add(sub)
         return final_set
+
